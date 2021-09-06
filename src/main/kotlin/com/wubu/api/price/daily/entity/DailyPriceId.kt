d56871bdf97280a35aa49c1e3268e8055a4bae5f
luckyDaveKim <lucky_dave@naver.com>
2021-09-06 20:05:19 +0900

    feat: Add column info

--- src/main/kotlin/com/wubu/api/price/daily/entity/DailyPriceId.kt
+++ src/main/kotlin/com/wubu/api/price/daily/entity/DailyPriceId.kt
@@ -4,13 +4,16 @@ import com.wubu.api.price.daily.model.Code
 import com.wubu.api.price.daily.model.Code.CodeConverter
 import java.io.Serializable
 import java.time.LocalDate
+import javax.persistence.Column
 import javax.persistence.Convert
 import javax.persistence.Embeddable
 
 @Embeddable
 data class DailyPriceId(
+        @Column(name = "code", nullable = false)
         @Convert(converter = CodeConverter::class)
         var code: Code,
 
+        @Column(name = "date", nullable = false)
         var date: LocalDate
 ) : Serializable
