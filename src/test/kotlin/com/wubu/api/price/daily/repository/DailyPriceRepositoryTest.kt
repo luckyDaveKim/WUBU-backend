c3a53699b0eb08a28f829adbfbc502151feb9f07
luckyDaveKim <lucky_dave@naver.com>
2021-09-06 20:05:48 +0900

    feat: Remove unused annotation

--- src/test/kotlin/com/wubu/api/price/daily/repository/DailyPriceRepositoryTest.kt
+++ src/test/kotlin/com/wubu/api/price/daily/repository/DailyPriceRepositoryTest.kt
@@ -8,16 +8,13 @@ import com.wubu.api.price.daily.model.Volume
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.LocalDate
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
 @ContextConfiguration(initializers = [ConfigDataApplicationContextInitializer::class])
 @ActiveProfiles("test")
