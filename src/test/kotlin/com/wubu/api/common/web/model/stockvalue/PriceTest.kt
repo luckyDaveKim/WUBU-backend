1275591178228786d5a6278222b7bfe8bcd1658a
luckyDaveKim <lucky_dave@naver.com>
2021-09-14 09:20:52 +0900

    fix: Fix stockValue test

--- src/test/kotlin/com/wubu/api/common/web/model/stockvalue/PriceTest.kt
+++ src/test/kotlin/com/wubu/api/common/web/model/stockvalue/PriceTest.kt
@@ -1,6 +1,7 @@
 package com.wubu.api.common.web.model.stockvalue
 
 import com.wubu.api.common.error.exception.InvalidPriceException
+import com.wubu.api.common.web.model.SingleValue
 import com.wubu.api.common.web.model.stockvalue.Price.PriceConverter
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
@@ -10,79 +11,138 @@ class PriceTest {
 
     @Test
     fun `생성 테스트`() {
+        // given
         val priceValue = 123456L
+
+        // when
         val price = Price(priceValue)
 
+        // then
         assertThat(price.value).isEqualTo(priceValue)
     }
 
     @Test
     fun `가격이 음수인 오류 테스트`() {
-        Assertions.assertThatThrownBy { Price(-1) }
+        // given
+        val priceValue = -1L
+
+        // when
+
+        // then
+        Assertions.assertThatThrownBy { Price(priceValue) }
                 .isInstanceOf(InvalidPriceException::class.java)
     }
 
     @Test
     fun `동등성 비교 테스트`() {
+        // given
         val priceValue = 1L
+
+        // when
         val price1 = Price(priceValue)
         val price2 = Price(priceValue)
 
+        // then
         assertThat(price1).isEqualTo(price2)
         assertThat(price1.value).isEqualTo(priceValue)
         assertThat(price2.value).isEqualTo(priceValue)
     }
 
     @Test
+    fun `SingleValue 인터페이스 테스트`() {
+        // given
+        val priceValue = 123456L
+
+        // when
+        val singleValue: SingleValue<Long> = Price(priceValue)
+
+        // then
+        assertThat(singleValue.value).isEqualTo(priceValue)
+    }
+
+    @Test
+    fun `StockValue 인터페이스 테스트`() {
+        // given
+        val priceValue = 123456L
+
+        // when
+        val stockValue: StockValue = Price(priceValue)
+
+        // then
+        assertThat(stockValue).isNotNull
+    }
+
+    @Test
     fun `DB to Object 테스트`() {
+        // given
         val dbPriceValue = 100L
+
+        // when
         val price = PriceConverter().convertToEntityAttribute(dbPriceValue)
 
-        assertThat(price).isNotNull
+        // then
         assertThat(price.value).isEqualTo(dbPriceValue)
     }
 
     @Test
     fun `DB to Object 0 값 테스트`() {
+        // given
         val dbPriceValue = 0L
+
+        // when
         val price = PriceConverter().convertToEntityAttribute(dbPriceValue)
 
-        assertThat(price).isNotNull
+        // then
         assertThat(price.value).isEqualTo(dbPriceValue)
     }
 
     @Test
     fun `DB to Object Null 값 테스트`() {
+        // given
         val dbPriceValue = null
+
+        // when
         val price = PriceConverter().convertToEntityAttribute(dbPriceValue)
 
-        assertThat(price).isNotNull
+        // then
         assertThat(price.value).isEqualTo(0)
     }
 
     @Test
     fun `Object to DB 테스트`() {
+        // given
         val priceValue = 100L
         val price = Price(priceValue)
+
+        // when
         val dbPrice = PriceConverter().convertToDatabaseColumn(price)
 
+        // then
         assertThat(dbPrice).isEqualTo(priceValue)
     }
 
     @Test
     fun `Object to DB 0 값 테스트`() {
+        // given
         val priceValue = 0L
         val price = Price(priceValue)
+
+        // when
         val dbPrice = PriceConverter().convertToDatabaseColumn(price)
 
+        // then
         assertThat(dbPrice).isEqualTo(priceValue)
     }
 
     @Test
     fun `Object to DB Null 값 테스트`() {
+        // given
         val price = null
+
+        // when
         val dbPrice = PriceConverter().convertToDatabaseColumn(price)
 
+        // then
         assertThat(dbPrice).isEqualTo(0)
     }
 
--- src/test/kotlin/com/wubu/api/common/web/model/stockvalue/VolumeTest.kt
+++ src/test/kotlin/com/wubu/api/common/web/model/stockvalue/VolumeTest.kt
@@ -1,6 +1,7 @@
 package com.wubu.api.common.web.model.stockvalue
 
 import com.wubu.api.common.error.exception.InvalidVolumeException
+import com.wubu.api.common.web.model.SingleValue
 import com.wubu.api.common.web.model.stockvalue.Volume.VolumeConverter
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
@@ -10,79 +11,138 @@ class VolumeTest {
 
     @Test
     fun `생성 테스트`() {
+        // given
         val volumeValue = 123456L
-        val volume = Price(volumeValue)
 
+        // when
+        val volume = Volume(volumeValue)
+
+        // then
         assertThat(volume.value).isEqualTo(volumeValue)
     }
 
     @Test
     fun `거래량이 음수인 오류 테스트`() {
-        Assertions.assertThatThrownBy { Volume(-1) }
+        // given
+        val volumeValue = -1L
+
+        // when
+
+        // then
+        Assertions.assertThatThrownBy { Volume(volumeValue) }
                 .isInstanceOf(InvalidVolumeException::class.java)
     }
 
     @Test
     fun `동등성 비교 테스트`() {
+        // given
         val volumeValue = 1L
-        val volume1 = Price(volumeValue)
-        val volume2 = Price(volumeValue)
 
+        // when
+        val volume1 = Volume(volumeValue)
+        val volume2 = Volume(volumeValue)
+
+        // then
         assertThat(volume1).isEqualTo(volume2)
         assertThat(volume1.value).isEqualTo(volumeValue)
         assertThat(volume2.value).isEqualTo(volumeValue)
     }
 
     @Test
+    fun `SingleValue 인터페이스 테스트`() {
+        // given
+        val volumeValue = 123456L
+
+        // when
+        val singleValue: SingleValue<Long> = Volume(volumeValue)
+
+        // then
+        assertThat(singleValue.value).isEqualTo(volumeValue)
+    }
+
+    @Test
+    fun `StockValue 인터페이스 테스트`() {
+        // given
+        val volumeValue = 123456L
+
+        // when
+        val stockValue: StockValue = Volume(volumeValue)
+
+        // then
+        assertThat(stockValue).isNotNull
+    }
+
+    @Test
     fun `DB to Object 테스트`() {
+        // given
         val volumeValue = 100L
+
+        // when
         val volume = VolumeConverter().convertToEntityAttribute(volumeValue)
 
-        assertThat(volume).isNotNull
+        // then
         assertThat(volume.value).isEqualTo(volumeValue)
     }
 
     @Test
     fun `DB to Object 0 값 테스트`() {
+        // given
         val volumeValue = 0L
+
+        // when
         val volume = VolumeConverter().convertToEntityAttribute(volumeValue)
 
-        assertThat(volume).isNotNull
+        // then
         assertThat(volume.value).isEqualTo(volumeValue)
     }
 
     @Test
     fun `DB to Object Null 값 테스트`() {
+        // given
         val volumeValue = null
+
+        // when
         val volume = VolumeConverter().convertToEntityAttribute(volumeValue)
 
-        assertThat(volume).isNotNull
+        // then
         assertThat(volume.value).isEqualTo(0)
     }
 
     @Test
     fun `Object to DB 테스트`() {
+        // given
         val volumeValue = 100L
         val volume = Volume(volumeValue)
+
+        // when
         val dbVolume = VolumeConverter().convertToDatabaseColumn(volume)
 
+        // then
         assertThat(dbVolume).isEqualTo(volumeValue)
     }
 
     @Test
     fun `Object to DB 0 값 테스트`() {
+        // given
         val volumeValue = 0L
         val volume = Volume(volumeValue)
+
+        // when
         val dbVolume = VolumeConverter().convertToDatabaseColumn(volume)
 
+        // then
         assertThat(dbVolume).isEqualTo(volumeValue)
     }
 
     @Test
     fun `Object to DB Null 값 테스트`() {
+        // given
         val volume = null
+
+        // when
         val dbVolume = VolumeConverter().convertToDatabaseColumn(volume)
 
+        // then
         assertThat(dbVolume).isEqualTo(0)
     }
 
