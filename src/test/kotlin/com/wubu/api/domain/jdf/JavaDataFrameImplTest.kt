e107062785617b655ca1d1b69e2988697877e042
luckyDaveKim <lucky_dave@naver.com>
2021-12-08 15:58:15 +0900

    feat: Create jdf test
    create with empty data

--- src/test/kotlin/com/wubu/api/domain/jdf/JavaDataFrameImplTest.kt
+++ src/test/kotlin/com/wubu/api/domain/jdf/JavaDataFrameImplTest.kt
@@ -18,6 +18,18 @@ internal class JavaDataFrameImplTest {
     }
 
     @Test
+    fun `empty data 생성 테스트`() {
+        // given
+        val data = emptyList<Double>()
+
+        // when
+        val jdf = JavaDataFrameImpl(data)
+
+        // then
+        assertThat(jdf).isNotNull
+    }
+
+    @Test
     fun `ewm 테스트`() {
         // given
         val data = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
