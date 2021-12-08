42b66984ea9c31db27045807caacd77bd4e25ac4
luckyDaveKim <lucky_dave@naver.com>
2021-12-08 15:49:15 +0900

    refactor: Refactor ewm

--- src/main/kotlin/com/wubu/api/domain/jdf/JavaDataFrameImpl.kt
+++ src/main/kotlin/com/wubu/api/domain/jdf/JavaDataFrameImpl.kt
@@ -1,10 +1,32 @@
 package com.wubu.api.domain.jdf
 
+import kotlin.math.pow
+import kotlin.math.roundToLong
+
 data class JavaDataFrameImpl(
     override val data: List<Double>
 ) : JavaDataFrame {
 
     override fun ewm(window: Int): List<Double> {
-        return listOf(1.0, 1.67, 2.56, 3.52, 4.51)
+        val result = mutableListOf(data[0])
+
+        for (i: Int in 1 until data.size) {
+            val beforeIndex = i - 1
+            val avg = (result[beforeIndex] * 0.33) + (data[i] * 0.67)
+            result.add(trimNum(avg))
+        }
+
+        return result
+    }
+
+    private fun trimNum(num: Double): Double {
+        val decimal = 2
+        val forDecimalNum = (10.0).pow(decimal)
+
+        var result = num * forDecimalNum
+        result = result.roundToLong().toDouble()
+        result /= forDecimalNum
+
+        return result
     }
 }
