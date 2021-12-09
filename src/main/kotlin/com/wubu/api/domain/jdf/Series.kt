f683a9b1f081faaf0c922a29bb2322e3198f79ba
luckyDaveKim <lucky_dave@naver.com>
2021-12-09 18:21:38 +0900

    refactor: Refactor plus series test

--- src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
+++ src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
@@ -1,9 +1,18 @@
 package com.wubu.api.domain.jdf
 
+import kotlin.math.max
+
 class Series(
     val data: List<Double>
 ) {
     operator fun plus(otherSeries: Series): Series {
-        return Series(listOf(2.0, 4.0, 6.0, 8.0, 10.0))
+        val newData = mutableListOf<Double>()
+        val maxSize = max(data.size, otherSeries.data.size)
+        for (i in 0 until maxSize) {
+            val curVal = data.getOrElse(i) { 0.0 }
+            val otherVal = otherSeries.data.getOrElse(i) { 0.0 }
+            newData.add(curVal + otherVal)
+        }
+        return Series(newData)
     }
 }
