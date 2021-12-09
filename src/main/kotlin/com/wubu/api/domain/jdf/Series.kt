a4bb40ea2d151ca3182ceffab2ab61e55b63b790
luckyDaveKim <lucky_dave@naver.com>
2021-12-09 18:28:28 +0900

    refactor: Refactor minus series test

--- src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
+++ src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
@@ -17,6 +17,13 @@ class Series(
     }
 
     operator fun minus(otherSeries: Series): Series {
-        return Series(listOf(0.0, 0.0, 0.0, 0.0, 0.0))
+        val newData = mutableListOf<Double>()
+        val maxSize = max(data.size, otherSeries.data.size)
+        for (i in 0 until maxSize) {
+            val curVal = data.getOrElse(i) { 0.0 }
+            val otherVal = otherSeries.data.getOrElse(i) { 0.0 }
+            newData.add(curVal - otherVal)
+        }
+        return Series(newData)
     }
 }
