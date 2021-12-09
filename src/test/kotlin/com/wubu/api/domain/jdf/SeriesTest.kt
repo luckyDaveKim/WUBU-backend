675698d5ff616c9f84953cbc0e8be01db0cf1450
luckyDaveKim <lucky_dave@naver.com>
2021-12-09 18:23:21 +0900

    feat: Success create empty data series test

--- src/test/kotlin/com/wubu/api/domain/jdf/SeriesTest.kt
+++ src/test/kotlin/com/wubu/api/domain/jdf/SeriesTest.kt
@@ -18,6 +18,18 @@ internal class SeriesTest {
     }
 
     @Test
+    fun `empty data 생성 테스트`() {
+        // given
+        val data = emptyList<Double>()
+
+        // when
+        val series = Series(data)
+
+        // then
+        assertThat(series).isNotNull
+    }
+
+    @Test
     fun `data 조회 테스트`() {
         // given
         val data = listOf(1.0, 2.0, 3.0, 4.0, 5.0)
