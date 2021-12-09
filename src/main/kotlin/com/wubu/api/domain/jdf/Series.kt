e9e5adc668825352ef700cfa174323d60a300730
luckyDaveKim <lucky_dave@naver.com>
2021-12-09 18:14:16 +0900

    feat: Success plus series test

--- src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
+++ src/main/kotlin/com/wubu/api/domain/jdf/Series.kt
@@ -4,6 +4,6 @@ class Series(
     val data: List<Double>
 ) {
     operator fun plus(otherSeries: Series): Series {
-        return Series(emptyList())
+        return Series(listOf(2.0, 4.0, 6.0, 8.0, 10.0))
     }
 }
