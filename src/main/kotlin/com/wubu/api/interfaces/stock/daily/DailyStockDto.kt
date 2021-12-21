d0e08b37a150c00fdb00e002fad3ffb6d2667a79
luckyDaveKim <lucky_dave@naver.com>
2021-12-22 01:21:55 +0900

    feat: Success dailyStockDto create test

--- src/main/kotlin/com/wubu/api/interfaces/stock/daily/DailyStockDto.kt
+++ src/main/kotlin/com/wubu/api/interfaces/stock/daily/DailyStockDto.kt
@@ -3,14 +3,10 @@ package com.wubu.api.interfaces.stock.daily
 import com.wubu.api.common.web.model.Point
 import com.wubu.api.domain.stock.StockPiece
 
-class DailyStockDto(stockPieces: List<StockPiece>) {
+class DailyStockDto(
+    stockPieces: List<StockPiece>
+) {
 
-    val prices: List<Point>
-        get() {
-            TODO()
-        }
-    val volumes: List<Point>
-        get() {
-            TODO()
-        }
+    val prices: List<Point> = stockPieces.map { Point.ofPrice(it) }
+    val volumes: List<Point> = stockPieces.map { Point.ofVolume(it) }
 }
