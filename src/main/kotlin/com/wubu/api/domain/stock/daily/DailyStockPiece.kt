37d422497fe4dee0f3a1e3f006d143abbc391bae
luckyDaveKim <lucky_dave@naver.com>
2021-12-21 23:16:14 +0900

    feat: Success dailyStockPiece test

--- src/main/kotlin/com/wubu/api/domain/stock/daily/DailyStockPiece.kt
+++ src/main/kotlin/com/wubu/api/domain/stock/daily/DailyStockPiece.kt
@@ -4,12 +4,13 @@ import com.wubu.api.common.web.model.OHLC
 import com.wubu.api.common.web.model.stockvalue.Volume
 import com.wubu.api.domain.stock.StockPiece
 import java.time.LocalDate
+import java.time.ZoneOffset
 
 class DailyStockPiece(
-    val date: LocalDate,
+    private val date: LocalDate,
     override val price: OHLC,
     override val volume: Volume
 ) : StockPiece {
     override val x: Number
-        get() = TODO("Not yet implemented")
+        get() = date.atStartOfDay().atZone(ZoneOffset.UTC).toInstant().toEpochMilli()
 }
