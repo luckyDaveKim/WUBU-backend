3d85a6d84f7c203ed51d3068abcdd36fbff2bb6e
luckyDaveKim <lucky_dave@naver.com>
2021-12-21 23:13:54 +0900

    feat: Success ohlc not equals test

--- src/test/kotlin/com/wubu/api/common/web/model/OHLCTest.kt
+++ src/test/kotlin/com/wubu/api/common/web/model/OHLCTest.kt
@@ -55,4 +55,35 @@ internal class OHLCTest {
         // then
         assertThat(ohlc1).isEqualTo(ohlc2)
     }
+
+    @Test
+    fun `동등성 비교 실패 테스트`() {
+        // given
+        val open1 = Price(1L)
+        val high1 = Price(2L)
+        val low1 = Price(3L)
+        val close1 = Price(4L)
+
+        val open2 = Price(5L)
+        val high2 = Price(6L)
+        val low2 = Price(7L)
+        val close2 = Price(8L)
+
+        // when
+        val ohlc1 = OHLC(
+            open = open1,
+            high = high1,
+            low = low1,
+            close = close1
+        )
+        val ohlc2 = OHLC(
+            open = open2,
+            high = high2,
+            low = low2,
+            close = close2
+        )
+
+        // then
+        assertThat(ohlc1).isNotEqualTo(ohlc2)
+    }
 }
