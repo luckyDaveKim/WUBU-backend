189e313e04cc8b67cdbf6a3e4093a9cf351c1584
luckyDaveKim <lucky_dave@naver.com>
2021-12-24 01:02:16 +0900

    refactor: Refactor duplicated value

--- src/test/kotlin/com/wubu/api/domain/stock/daily/DailyStockPieceTest.kt
+++ src/test/kotlin/com/wubu/api/domain/stock/daily/DailyStockPieceTest.kt
@@ -6,102 +6,106 @@ import com.wubu.api.common.web.model.stockvalue.Volume
 import com.wubu.api.domain.stock.StockPiece
 import com.wubu.api.domain.stock.StockPrice
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.ZoneOffset
 
 internal class DailyStockPieceTest {
 
-    @Test
-    fun `생성 테스트`() {
-        // given
-        val companyCode = CompanyCode("000001")
-        val date = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId = DailyStockPieceId(
-            companyCode = companyCode,
-            date = date
+    private lateinit var companyCode1: CompanyCode
+    private lateinit var companyCode2: CompanyCode
+    private lateinit var date1: LocalDate
+    private lateinit var date2: LocalDate
+    private lateinit var dailyStockPieceId1: DailyStockPieceId
+    private lateinit var dailyStockPieceId2: DailyStockPieceId
+    private lateinit var price1: StockPrice
+    private lateinit var price2: StockPrice
+    private lateinit var volume1: Volume
+    private lateinit var volume2: Volume
+
+    @BeforeEach
+    fun setUp() {
+        companyCode1 = CompanyCode("000001")
+        date1 = LocalDate.of(1991, 3, 26)
+        dailyStockPieceId1 = DailyStockPieceId(
+            companyCode = companyCode1,
+            date = date1
         )
-        val price = StockPrice(
+        price1 = StockPrice(
             open = Price(1L),
             high = Price(2L),
             low = Price(3L),
             close = Price(4L)
         )
-        val volume = Volume(5L)
+        volume1 = Volume(5L)
+
+        companyCode2 = CompanyCode("000002")
+        date2 = LocalDate.of(1991, 3, 27)
+        dailyStockPieceId2 = DailyStockPieceId(
+            companyCode = companyCode2,
+            date = date2
+        )
+        price2 = StockPrice(
+            open = Price(10L),
+            high = Price(20L),
+            low = Price(30L),
+            close = Price(40L)
+        )
+        volume2 = Volume(50L)
+    }
+
+    @Test
+    fun `생성 테스트`() {
+        // given
 
         // when
         val dailyStockPiece = DailyStockPiece(
-            id = dailyStockPieceId,
-            price = price,
-            volume = volume
+            id = dailyStockPieceId1,
+            price = price1,
+            volume = volume1
         )
 
         // then
         assertThat(dailyStockPiece).isNotNull
-        assertThat(dailyStockPiece.id).isEqualTo(dailyStockPieceId)
-        assertThat(dailyStockPiece.x).isEqualTo(date.atStartOfDay().atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
-        assertThat(dailyStockPiece.price).isEqualTo(price)
-        assertThat(dailyStockPiece.volume).isEqualTo(volume)
+        assertThat(dailyStockPiece.id).isEqualTo(dailyStockPieceId1)
+        assertThat(dailyStockPiece.x).isEqualTo(date1.atStartOfDay().atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
+        assertThat(dailyStockPiece.price).isEqualTo(price1)
+        assertThat(dailyStockPiece.volume).isEqualTo(volume1)
     }
 
     @Test
     fun `StockPiece 구현 테스트`() {
         // given
-        val companyCode = CompanyCode("000001")
-        val date = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId = DailyStockPieceId(
-            companyCode = companyCode,
-            date = date
-        )
-        val price = StockPrice(
-            open = Price(1L),
-            high = Price(2L),
-            low = Price(3L),
-            close = Price(4L)
-        )
-        val volume = Volume(5L)
 
         // when
         val stockPiece: StockPiece = DailyStockPiece(
-            id = dailyStockPieceId,
-            price = price,
-            volume = volume
+            id = dailyStockPieceId1,
+            price = price1,
+            volume = volume1
         )
 
         // then
         assertThat(stockPiece).isNotNull
-        assertThat(stockPiece.x).isEqualTo(date.atStartOfDay().atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
-        assertThat(stockPiece.price).isEqualTo(price)
-        assertThat(stockPiece.volume).isEqualTo(volume)
+        assertThat(stockPiece.x).isEqualTo(date1.atStartOfDay().atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
+        assertThat(stockPiece.price).isEqualTo(price1)
+        assertThat(stockPiece.volume).isEqualTo(volume1)
     }
 
     @Test
     fun `동등성 비교 테스트`() {
         // given
-        val companyCode = CompanyCode("000001")
-        val date = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId = DailyStockPieceId(
-            companyCode = companyCode,
-            date = date
-        )
-        val price = StockPrice(
-            open = Price(1L),
-            high = Price(2L),
-            low = Price(3L),
-            close = Price(4L)
-        )
-        val volume = Volume(5L)
 
         // when
         val dailyStockPiece1 = DailyStockPiece(
-            id = dailyStockPieceId,
-            price = price,
-            volume = volume
+            id = dailyStockPieceId1,
+            price = price1,
+            volume = volume1
         )
         val dailyStockPiece2 = DailyStockPiece(
-            id = dailyStockPieceId,
-            price = price,
-            volume = volume
+            id = dailyStockPieceId1,
+            price = price1,
+            volume = volume1
         )
 
         // then
@@ -109,28 +113,8 @@ internal class DailyStockPieceTest {
     }
 
     @Test
-    fun `다른 date 동등성 비교 실패 테스트`() {
+    fun `다른 id 동등성 비교 실패 테스트`() {
         // given
-        val companyCode1 = CompanyCode("000001")
-        val date1 = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId1 = DailyStockPieceId(
-            companyCode = companyCode1,
-            date = date1
-        )
-        val price1 = StockPrice(
-            open = Price(1L),
-            high = Price(2L),
-            low = Price(3L),
-            close = Price(4L)
-        )
-        val volume1 = Volume(5L)
-
-        val companyCode2 = CompanyCode("000002")
-        val date2 = LocalDate.of(1991, 3, 27)
-        val dailyStockPieceId2 = DailyStockPieceId(
-            companyCode = companyCode2,
-            date = date2
-        )
 
         // when
         val dailyStockPiece1 = DailyStockPiece(
@@ -151,26 +135,6 @@ internal class DailyStockPieceTest {
     @Test
     fun `다른 price 동등성 비교 실패 테스트`() {
         // given
-        val companyCode = CompanyCode("000001")
-        val date = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId1 = DailyStockPieceId(
-            companyCode = companyCode,
-            date = date
-        )
-        val price1 = StockPrice(
-            open = Price(1L),
-            high = Price(2L),
-            low = Price(3L),
-            close = Price(4L)
-        )
-        val volume1 = Volume(5L)
-
-        val price2 = StockPrice(
-            open = Price(5L),
-            high = Price(6L),
-            low = Price(7L),
-            close = Price(8L)
-        )
 
         // when
         val dailyStockPiece1 = DailyStockPiece(
@@ -191,21 +155,6 @@ internal class DailyStockPieceTest {
     @Test
     fun `다른 volume 동등성 비교 실패 테스트`() {
         // given
-        val companyCode = CompanyCode("000001")
-        val date = LocalDate.of(1991, 3, 26)
-        val dailyStockPieceId1 = DailyStockPieceId(
-            companyCode = companyCode,
-            date = date
-        )
-        val price1 = StockPrice(
-            open = Price(1L),
-            high = Price(2L),
-            low = Price(3L),
-            close = Price(4L)
-        )
-        val volume1 = Volume(5L)
-
-        val volume2 = Volume(6L)
 
         // when
         val dailyStockPiece1 = DailyStockPiece(
