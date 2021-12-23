421f5db411e0633ee9ef20288bb4a820699e4ab1
luckyDaveKim <lucky_dave@naver.com>
2021-12-23 22:51:57 +0900

    feat: Success DailyStockPieceId equals test

--- src/main/kotlin/com/wubu/api/domain/stock/daily/DailyStockPieceId.kt
+++ src/main/kotlin/com/wubu/api/domain/stock/daily/DailyStockPieceId.kt
@@ -3,7 +3,7 @@ package com.wubu.api.domain.stock.daily
 import com.wubu.api.common.web.model.CompanyCode
 import java.time.LocalDate
 
-class DailyStockPieceId(
+data class DailyStockPieceId(
     var companyCode: CompanyCode,
     var date: LocalDate
 )
